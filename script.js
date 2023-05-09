9b28be8d00edf64b45564e15712a9d2ebf9985f3
Lord <122161005+LordBenamar@users.noreply.github.com>
2023-05-09 10:01:27 -0400

    archivos
    portafolio

---
+++ script.js
@@ -0,0 +1,77 @@
+let menuVisible = false;
+//oculta o muestra el menu
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
+function seleccionar(){
+    //oculto el menu una vez que selecciono una opcion
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
+//Funcion que aplica las animaciones de las habilidades
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("javascript");
+        habilidades[1].classList.add("htmlcss");
+        habilidades[2].classList.add("photoshop");
+        habilidades[3].classList.add("wordpress");
+        habilidades[4].classList.add("drupal");
+        habilidades[5].classList.add("comunicacion");
+        habilidades[6].classList.add("trabajo");
+        habilidades[7].classList.add("creatividad");
+        habilidades[8].classList.add("dedicacion");
+        habilidades[9].classList.add("proyect");
+    }
+}
+
+//scrolling para aplicar la animacion de la barra de habilidades
+window.onscroll = function(){
+    efectoHabilidades();
+} 
+
+const time = document.getElementById('time');
+const date = document.getElementById('date');
+
+const monthNames = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+    "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
+];
+
+const interval = setInterval(() => {
+
+    const local = new Date();
+    
+    let day = local.getDate(),
+        month = local.getMonth(),
+        year = local.getFullYear();
+
+    time.innerHTML = local.toLocaleTimeString();
+    date.innerHTML = `${day} ${monthNames[month]} ${year}`;
+
+}, 1000);
+
+
+function validar(){
+	var form =document.form
+	if(form.nombre.value ==0){
+		alert("Ingresar su  nombre y apellido")
+		form.nombre.value="";
+		form.nombre.focus()
+		return false;
+	}
+
+	if(form.correo.value ==0){
+		alert("Ingresar correo electronico")
+		form.correo.value="";
+		form.correo.focus()
+		return false;
+	}
+}	
